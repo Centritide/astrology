d6641e8dcba059c32745b83e55fb6e3ed5a42b2c
Lilac Chang <mail@teagrrl.dev>
2021-05-21 19:08:31 -0400

    added ballpark modification support (just fifth base, grind rail, and secret base for now), added control toggles

--- ballparks/main.js
+++ ballparks/main.js
@@ -30,7 +30,7 @@ requirejs.config({
 });
 //Start the main app logic.
 requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbone) {
-	var App = {Models: {}, Collections: {}, Views: {}, Router: {}}, activeRouter, stadiumView, colors = { base: "#fff", dirt: "#c49463", fort: "#aaa", foul: "#fff", grass: "#4a8640", stroke: "#000" };
+	var App = {Models: {}, Collections: {}, Views: {}, Router: {}}, activeRouter, stadiumView, colors = { base: "#fff", dirt: "#c49463", fort: "#aaa", foul: "#fff", grass: "#4a8640", rail: "#ccc", stroke: "#000" }, lightMode;
 	
 	//-- BEGIN ROUTER --
 	App.Router = Backbone.Router.extend({
@@ -80,13 +80,17 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 		},
 		render: function() {
 			this.$el.html(this.template({ 
-				fields: this.getFields(), 
+				fields: this.getFields(),
+				modifications: this.getModifications(), 
 				stadiums: this.collection
 			}));
 			this.resize();
 		},
 		getFields: function() {
-			return ["grandiosity", "fortification", "obtuseness", "forwardness", "elongation"];
+			return ["elongation", "forwardness", "obtuseness", "grandiosity", "fortification"];
+		},
+		getModifications: function() {
+			return ["fifth base", "grind rail", "secret base"];
 		},
 		draw: function() {
 			this.$el.find("canvas").clearCanvas();
@@ -97,7 +101,10 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 				this.$el.find("[data-control=fortification] .input-number").val(), 
 				this.$el.find("[data-control=obtuseness] .input-number").val(), 
 				this.$el.find("[data-control=forwardness] .input-number").val(), 
-				this.$el.find("[data-control=elongation] .input-number").val()
+				this.$el.find("[data-control=elongation] .input-number").val(),
+				_.chain(this.$el.find(".modifications-group label")).map(function(checkbox) {
+					return $(checkbox).find(".input-checkbox").prop("checked") ? $(checkbox).data("control") : null;
+				}).compact().value()
 			);
 		},
 		resize: function() {
@@ -109,9 +116,14 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 			this.draw();
 		},
 		events: {
+			"input .input-checkbox": "changeCheckbox",
 			"input .input-slider": "changeSlider",
 			"input .input-number": "changeNumber",
-			"change .stadium-selector select": "loadStadiumStats"
+			"change .stadium-selector select": "selectStadium",
+			"click .controls-toggles a": "clickToggle"
+		},
+		changeCheckbox: function(e) {
+			this.draw();
 		},
 		changeSlider: function(e) {
 			$(e.currentTarget).siblings(".input-number").val($(e.currentTarget).val() / 1000);
@@ -131,7 +143,7 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 			this.$el.find("select").val("none");
 			this.draw();
 		},
-		loadStadiumStats: function(e) {
+		selectStadium: function(e) {
 			var thisView = this, foundStadium = this.collection.get($(e.currentTarget).val());
 			if(foundStadium) {
 				_.each(this.getFields(), function(field) {
@@ -139,13 +151,40 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 					thisView.$el.find("[data-control=" + field + "] .input-slider").val(fieldValue);
 					thisView.$el.find("[data-control=" + field + "] .input-number").val(fieldValue / 1000);
 				});
+				_.each(this.getModifications(), function(mod) {
+					mod = mod.replace(/\s+/, "_").toUpperCase();
+					thisView.$el.find("[data-control=" + mod + "] .input-checkbox").prop("checked", _.contains(foundStadium.get("mods"), mod));
+				});
 				thisView.draw();
+			}
+		},
+		clickToggle: function(e) {
+			e.preventDefault();
+			switch($(e.currentTarget).data("toggle")) {
+				case "controls":
+					this.$el.find(".controls-container").toggle();
+					break;
+				case "lights":
+					lightMode = $("main").hasClass("dark");
+					$("main").addClass("transition");
+					$("main").toggleClass("dark", !lightMode);
+					if(localStorageExists()) {
+						localStorage.setItem("lightModeState", lightMode);
+					}
+					break;
 			}
 		}
 	});
 	//-- END VIEWS --
+
+	//-- CHECK FOR SAVED DATA --
+	if(localStorageExists()) {
+		if(localStorage.getItem("lightModeState") != null) {
+			lightMode = localStorage.getItem("lightModeState") == "true";
+		}
+	}
 	
-	//$("#root").toggleClass("dark", !lightMode);
+	$("main").toggleClass("dark", !lightMode);
 	
 	activeRouter = new App.Router;
 	Backbone.history.start();
@@ -165,31 +204,77 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 		});
 	}
 
-	function drawField(height, width, grandiosity, fortification, obtuseness, forwardness, elongation) {
-		var scale = Math.min(height / 70, width / 70),
+	function drawField(height, width, grandiosity, fortification, obtuseness, forwardness, elongation, modifications) {
+		var scale = Math.min(height / 80, width / 75),
 			stroke = Math.ceil(scale / 8),
 			backstopOffset = 5 * scale,
 			fortificationH = scale * 2 * (1 + fortification * 5) / 3,
 			obtusenessAngle = (3 + obtuseness * 4) * Math.PI / 10,
-			outfieldLength = backstopOffset + scale * 6 * (1 + grandiosity * 3),
+			outfieldLength = backstopOffset + scale * 4 * (4 + grandiosity * 6),
 			pathLength = 3 * scale * (2 + elongation * 3),
 			elongationX = pathLength * Math.sin(obtusenessAngle / 2), 
 			elongationY = pathLength * Math.cos(obtusenessAngle / 2),
-			forwardnessY = (forwardness - 0.5) * elongationY,
+			forwardnessY = (forwardness - 0.5) * (elongationY - scale / 2),
 			grandiosityX = (pathLength + outfieldLength) * Math.sin(obtusenessAngle / 2),
 			grandiosityY = (pathLength + outfieldLength) * Math.cos(obtusenessAngle / 2),
 			x = width / 2,
 			y = (height + fortificationH + grandiosityY - elongationY) / 2;
 		
 		drawGrass(x, y, backstopOffset, elongationY, grandiosityX, grandiosityY);
-		drawDirt(scale, x, y, y + forwardnessY, y + elongationY, x + elongationX, y - elongationY, x - elongationX, elongationY / 2, obtusenessAngle / 2, 2 * scale);
+		drawDirt(scale, x, y, y + forwardnessY, y + elongationY, x + elongationX, y - elongationY, x - elongationX, x - 2 * elongationX, elongationY / 2, obtusenessAngle / 2, 2 * scale, modifications);
 		drawFoulLine(x, y, elongationY, grandiosityX, grandiosityY, stroke);
 		drawHomePlate(x, y + elongationY, scale * 17 / 15, stroke);
-		drawBase(x + elongationX, y, scale, "right", stroke);
-		drawBase(x, y - elongationY, scale, null, stroke);
-		drawBase(x - elongationX, y, scale, "left", stroke);
+		drawBase(x + elongationX, y, scale, stroke, "right");
+		drawBase(x, y - elongationY, scale, stroke);
+		drawBase(x - elongationX, y, scale, stroke, "left");
 		drawMound(x, y + forwardnessY, scale, stroke);
 		drawFortifications(x, y, grandiosityX, elongationY + backstopOffset - grandiosityY, 2 * grandiosityY - backstopOffset, fortificationH);
+		_.each(modifications, function(mod) {
+			switch(mod) {
+				case "FIFTH_BASE":
+					drawBase(x - 2 * elongationX, y - elongationY, scale, stroke);
+					break;
+				case "GRIND_RAIL":
+					drawGrindRail(x, y, elongationX, elongationY, forwardnessY, scale, stroke);
+					break;
+				case "SECRET_BASE":
+					drawBase(x, y - elongationY - 2 * scale, scale, stroke, "secret");
+					break;
+				case "BIG_BUCKETS":
+				case "BIRDHOUSES":
+				case "CRIME_SCENE":
+				case "ECHO_CHAMBER":
+				case "EVENT_HORIZON":
+				case "FAX_MACHINE":
+				case "FIRE_INSURANCE":
+				case "FLOOD_PUMPS":
+				case "SOUNDSYSTEM":
+				case "HOOPS":
+				case "HOTEL_MOTEL":
+				case "PEANUT_MISTER":
+				case "PSYCHOACOUSTICS":
+				case "SMITHY":
+				case "SOLAR_PANELS":
+				case "SOUNDSYSTEM":
+				case "TURNTABLES":
+				default:
+					// eh maybe later
+					break;
+			}
+		});
+	}
+
+	function drawGrindRail(x, y, elongationX, elongationY, forwardnessY, scale, stroke) {
+		$("canvas").drawPath({
+			strokeStyle: colors.rail,
+			strokeWidth: stroke,
+			p1: {
+				type: 'quadratic',
+				x1: x - elongationX + 1.2 * scale, y1: y - scale / 2,
+				cx1: x, cy1: y - elongationY + forwardnessY,
+				x2: x + elongationX - 1.2 * scale, y2: y - scale / 2
+			}
+		});
 	}
 
 	function drawFortifications(x, y, fortificationX, fortificationY, fortificationC, fortificationH) {
@@ -232,22 +317,35 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 		});
 	}
 
-	function radToDeg(rad) {
-		return rad / Math.PI * 180;
-	}
-
-	function drawDirt(scale, centerX, centerY, moundY, homeY, firstX, secondY, thirdX, infieldR, theta, radius) {
+	function drawDirt(scale, centerX, centerY, moundY, homeY, firstX, secondY, thirdX, fifthX, infieldR, theta, radius, modifications) {
 		// draw baseline
-		$("canvas").drawLine({
+		var baseLineAttrs = {
 			strokeStyle: colors.dirt,
 			strokeWidth: 2 * scale / 3,
-			x1: centerX, y1: homeY,
-			x2: firstX, y2: centerY,
-			x3: centerX, y3: secondY,
-			x4: thirdX, y4: centerY,
-			x5: centerX, y5: homeY,
-			closed: true
-		});
+			x1: thirdX, y1: centerY,
+			x2: centerX, y2: homeY,
+			x3: firstX, y3: centerY,
+			x4: centerX, y4: secondY,
+			x5: thirdX, y5: centerY,
+			closed: false
+		};
+		if(_.contains(modifications, "FIFTH_BASE")) {
+			baseLineAttrs.x6 = fifthX;
+			baseLineAttrs.y6 = secondY;
+			$("canvas").drawArc({
+				fillStyle: colors.dirt,
+				x: fifthX, y: secondY,
+				radius: radius / 2
+			});
+		}
+		if(_.contains(modifications, "SECRET_BASE")) {
+			$("canvas").drawArc({
+				fillStyle: colors.dirt,
+				x: centerX, y: secondY - 2 * scale,
+				radius: radius
+			});
+		}
+		$("canvas").drawLine(baseLineAttrs);
 		// draw infied
 		$("canvas").drawPath({
 			fillStyle: colors.dirt,
@@ -352,7 +450,20 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 		});
 	}
 
-	function drawBase(x, y, size, offset, width) {
+	function drawBase(x, y, size, width, offset) {
+		var pathAttrs = {
+			strokeStyle: colors.stroke,
+			strokeWidth: width,
+			fillStyle: colors.base,
+			p1: {
+				type: 'line',
+				x1: x - size / 2, y1: y,
+				x2: x, y2: y + size / 2,
+				x3: x + size / 2, y3: y,
+				x4: x, y4: y - size / 2
+			},
+			closed: true
+		};
 		switch(offset) {
 			case "left":
 				offset = size / 2;
@@ -360,23 +471,17 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 			case "right":
 				offset = size / -2;
 				break;
+			case "secret":
+				pathAttrs.opacity = 0.4;
 			default:
 				offset = 0;
 				break;
 		}
-		$("canvas").drawPath({
-			strokeStyle: colors.stroke,
-			strokeWidth: width,
-			fillStyle: colors.base,
-			p1: {
-				type: 'line',
-				x1: x - size / 2 + offset, y1: y,
-				x2: x + offset, y2: y + size / 2,
-				x3: x + size / 2 + offset, y3: y,
-				x4: x + offset, y4: y - size / 2
-			},
-			closed: true
-		});
+		pathAttrs.p1.x1 += offset;
+		pathAttrs.p1.x2 += offset;
+		pathAttrs.p1.x3 += offset;
+		pathAttrs.p1.x4 += offset;
+		$("canvas").drawPath(pathAttrs);
 	}
 
 	function drawHomePlate(x, y, size, width) {
@@ -394,5 +499,20 @@ requirejs(["jquery", "underscore", "backbone", "jcanvas"], function($, _, Backbo
 			},
 			closed: true
 		});
+	}
+
+	function radToDeg(rad) {
+		return rad / Math.PI * 180;
+	}
+
+	function localStorageExists() {
+		if(window.localStorage){
+			window.localStorage.setItem("test", "test");
+			if(window.localStorage.getItem("test")){
+				window.localStorage.removeItem("test");
+				return true;
+			}
+		}
+		return false;
 	}
 });
